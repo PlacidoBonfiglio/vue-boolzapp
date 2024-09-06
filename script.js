e55477e5ae3306eac491e7171f92a56792871ef8
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-06 12:42:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todoList: [
+
+            ]
+        }
+    },
+
+    methods: {
+
+    }
+
+}).mount('#app');
